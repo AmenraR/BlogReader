04e66f5f9dfb0c9f00d9460158795b1331394c80
Ezra Alarid <eja.117@gmail.com>
2014-09-30 14:55:14 -0700

    added an adapter to BlogActivity on onCreate
    milestone I 5

--- app/src/main/java/android/alaride/blogreader/BlogActivity.java
+++ app/src/main/java/android/alaride/blogreader/BlogActivity.java
@@ -2,6 +2,7 @@ package android.alaride.blogreader;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 
@@ -16,12 +17,23 @@ public class BlogActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blog);
 
+        String[] arrayStrings = new String[]{
+                "Blog Post 1",
+                "Blog Post 2",
+                "Blog Post 3",
+                "Blog Post 4";
+        };
+
         progressBar = (ProgressBar)findViewById(R.id.progressBar);
         listView = (ListView)findViewById(R.id.ListView);
 
         //when listview is empty, the progress bar will show, if it isnt, it will disappear.
         listView.setEmptyView(progressBar);
 
+        //adapts the information in order to have it displayed into the listView
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayStrings);
+       //tells the listView to use the previously created adapter
+        listView.setAdapter(adapter);
     }
 
 
