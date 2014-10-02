b6a10eeeb07b26855caa525d32ffd27ba5bdf9d7
Ezra Alarid <eja.117@gmail.com>
2014-10-02 14:58:55 -0700

    adding url connection
    milestone I  10

--- app/src/main/java/android/alaride/blogreader/BlogPostTask.java
+++ app/src/main/java/android/alaride/blogreader/BlogPostTask.java
@@ -6,6 +6,8 @@ import android.util.Log;
 
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -16,11 +18,22 @@ public class BlogPostTask extends AsyncTask<Activity, Void, JSONObject>{
         //catches the errors in the Url statement and displaying them in the catch
         try {
             URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=10");
+
+            HttpURLConnection connection = (HttpURLConnection)blogFeedUrl.openConnection();
+            connection.connect();
+            int responseCode = connection.getResponseCode();
+
+            if(responseCode == HttpURLConnection.HTTP_OK){
+                
+            }
+
         }
         catch(MalformedURLException error){
-            Log.e("BlogPostTask", "Malformed URL: " + error)
+            Log.e("BlogPostTask", "Malformed URL: " + error);
         }
-
+        catch(IOException error){
+            Log.e("BlogPostTask", "IO Exception" + error);
+        }
         return null;
     }
 }
