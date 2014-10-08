e049da795792f41a3a603629935e9b1697cd15de
Ezra Alarid <eja.117@gmail.com>
2014-10-08 14:56:58 -0700

    milestone S 12 adding the BlogPostParser

---
+++ app/src/main/java/android/alaride/blogreader/BlogPostParser.java
@@ -0,0 +1,19 @@
+package android.alaride.blogreader;
+
+public class BlogPostParser {
+
+    private static BlogPostParser parser;
+
+    private BlogPostParser(){
+
+    }
+
+    public static BlogPostParser get(){
+
+        if(parser == null){
+            parser = new BlogPostParser();
+        }
+        return parser;
+    }
+
+}
