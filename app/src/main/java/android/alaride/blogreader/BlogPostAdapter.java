4ac8555137f1df0d46f38e2f76e2202b54bc76bb
Ezra Alarid <eja.117@gmail.com>
2014-10-22 18:53:03 -0700

    milestone S 29
    adding getView override into BlogPostAdapter

--- app/src/main/java/android/alaride/blogreader/BlogPostAdapter.java
+++ app/src/main/java/android/alaride/blogreader/BlogPostAdapter.java
@@ -1,6 +1,8 @@
 package android.alaride.blogreader;
 
 import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
@@ -8,6 +10,11 @@ import java.util.ArrayList;
 public class BlogPostAdapter extends ArrayAdapter<BlogPost>{
     public BlogPostAdapter(Context context, ArrayList<BlogPost> posts) {
         super(context, 0, posts);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
     }
 }
 //start at 28 at 2 minutes
